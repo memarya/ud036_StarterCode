bba7621e279edd89d2a40b2396741e4375c31333
unknown <memarya@gmail.com>
2017-11-09 22:37:11 -0500

    Bring to PEP8 compliance.

--- media.py
+++ media.py
@@ -1,12 +1,12 @@
 class Movie():
-	"""Movie class provides a data structure for storing movie information.
-    
-    Attributes:
-        movie_title: Movie title.
-        poster_image_url: A link to the movie poster image.
-        trailer_youtube_url: A youtube link to the movie trailer
+    """Movie class provides a data structure for storing movie information.
+
+       Attributes:
+          movie_title: Movie title.
+          poster_image_url: A link to the movie poster image.
+          trailer_youtube_url: A youtube link to the movie trailer
     """
-	def __init__(self, movie_title, poster_image_url, trailer_youtube_url):
-		self.title = movie_title
-		self.poster_image_url = poster_image_url
-		self.trailer_youtube_url = trailer_youtube_url
+    def __init__(self, movie_title, poster_image_url, trailer_youtube_url):
+        self.title = movie_title
+        self.poster_image_url = poster_image_url
+        self.trailer_youtube_url = trailer_youtube_url
